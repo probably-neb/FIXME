1950f862ba20e0f42160a504b2ba4981ac1b11e8
Ben Kunkle <ben.kunkle@gmail.com>
2024-09-26 18:06:23 -0500

    add config.zig

---
+++ src/config.zig
@@ -0,0 +1,51 @@
+const std = @import("std");
+const toml = @import("toml");
+
+pub const ConfigFile = struct {
+    name: []const u8,
+    api_key: []const u8,
+    label_names: ?LabelNames,
+};
+pub const Config = struct {
+    name: []const u8,
+    api_key: []const u8,
+    label_names: LabelNames,
+
+    pub fn format(self: *const Config, comptime _: []const u8, _: std.fmt.FormatOptions, writer: anytype) !void {
+        try writer.print("config.Config {{.name = \"{s}\", .api_key = \"{s}\", .label_names = {any} }}", .{ self.name, self.api_key, self.label_names });
+    }
+};
+
+pub const LabelNames = struct {
+    FIXME: ?[]const u8,
+    TODO: ?[]const u8,
+
+    pub fn label_name_for(label_names: *const @This(), issue_kind: @import("main.zig").IssueKind) []const u8 {
+        return switch (issue_kind) {
+            .FIXME => label_names.FIXME orelse "FIXME",
+            .TODO => label_names.TODO orelse "TODO",
+        };
+    }
+
+    pub fn format(self: *const LabelNames, comptime _: []const u8, _: std.fmt.FormatOptions, writer: anytype) !void {
+        try writer.print("config.LabelNames {{.FIXME = \"{s}\", .TODO = \"{s}\" }}", .{ self.label_name_for(.FIXME), self.label_name_for(.TODO) });
+    }
+};
+
+pub fn load(alloc: std.mem.Allocator) !Config {
+    var parser = toml.Parser(ConfigFile).init(alloc);
+    defer parser.deinit();
+
+    var result = try parser.parseFile("./fixme.toml");
+    defer result.deinit();
+
+    const config = Config{
+        .name = result.value.name,
+        .api_key = result.value.api_key,
+        .label_names = result.value.label_names orelse LabelNames{
+            .FIXME = null,
+            .TODO = null,
+        },
+    };
+    return config;
+}
