7715fb7e688e9c77ee1f7e05858da33cc0bdc332
Ben Kunkle <ben.kunkle@gmail.com>
2024-09-26 22:54:26 -0500

    add team key to config

--- src/config.zig
+++ src/config.zig
@@ -5,10 +5,12 @@ pub const ConfigFile = struct {
     name: []const u8,
     api_key: []const u8,
     label_names: ?LabelNames,
+    team_key: []const u8,
 };
 pub const Config = struct {
     name: []const u8,
     api_key: []const u8,
+    team_key: []const u8,
     label_names: LabelNames,
 
     pub fn format(self: *const Config, comptime _: []const u8, _: std.fmt.FormatOptions, writer: anytype) !void {
@@ -46,6 +48,7 @@ pub fn load(alloc: std.mem.Allocator) !Config {
             .FIXME = null,
             .TODO = null,
         },
+        .team_key = result.value.team_key,
     };
     return config;
 }
