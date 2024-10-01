3288d23a5ac3486bbf9fbe61d296b750f4a3bf83
Ben Kunkle <ben.kunkle@gmail.com>
2024-10-01 12:55:37 -0500

    add tests to build.zig

--- build.zig
+++ build.zig
@@ -33,4 +33,13 @@ pub fn build(b: *std.Build) void {
 
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
+
+    const exe_tests = b.addTest(.{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&exe_tests.step);
 }
